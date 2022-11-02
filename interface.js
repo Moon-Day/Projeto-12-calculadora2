179bdab1c61e66680cfcbf10fed0a092cf920a5c
Guilherme de Souza <97771245+Moon-Day@users.noreply.github.com>
2022-11-02 15:41:31 -0300

    termino

---
+++ interface.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+
+})
+function handleClick(event) {
+
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)) {
+
+        setTimeout(() => {
+
+            alert("O Jogo Acabou - O Vencedor foi " + playerTime)
+
+        }, 10);
+
+        
+
+    };
+    updateSquare(position);
+}
+function updateSquare(position) {
+
+    let square = document.getElementById(position.toString());
+    let symbols = board[position];
+    square.innerHTML = `<div class='${symbols}'></div>`;
+
+}
